5163157b1a772d51580dd9307c070c8a272b7c4b
Kyle LI <93997542+kyleli666@users.noreply.github.com>
2022-11-13 17:07:46 +0800

    Update update-china-list.sh
    sed用单引号避免命令解析异常。smartdns reload更新为restart。

--- update-china-list.sh
+++ update-china-list.sh
@@ -12,9 +12,9 @@ cat /tmp/smartdns/apple.conf >> /tmp/smartdns/china.conf 2>/dev/null
 cat /tmp/smartdns/google.conf >> /tmp/smartdns/china.conf 2>/dev/null
 
 #删除不符合规则的域名
-sed -i "s/^server=\/\(.*\)\/[^\/]*$/nameserver \/\1\/china/g;/^nameserver/!d" /tmp/smartdns/china.conf 2>/dev/null
+sed -i 's/^server=\/\(.*\)\/[^\/]*$/nameserver \/\1\/china/g;/^nameserver/!d' /tmp/smartdns/china.conf 2>/dev/null
 
 mv -f /tmp/smartdns/china.conf  /etc/smartdns/smartdns-domains.china.conf
 rm -rf /tmp/smartdns/
 
-/etc/init.d/smartdns reload
+/etc/init.d/smartdns restart
